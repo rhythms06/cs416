4ef7dc3b989c9b369ff0f657766cd5f4a1b75d8b
Sakib Rasul <rhythms06@gmail.com>
2020-09-20 19:58:42 -0300

    init TimeSignal.c

--- Proj1/TimeSignal.c
+++ Proj1/TimeSignal.c
@@ -1,23 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 
-// Place any necessary global variables here
+int numExceptions = 0;
+int maxExceptions = 100000;
 
-void handle_sigfpe(int signum){
+struct timeval start, end;
 
-	// Handler code goes here
+void handle_sigfpe(int signum) {
+	numExceptions++;
 
+	if (numExceptions == maxExceptions) {
+		gettimeofday(&end, NULL); // stop timer
+
+		long elapsedMicroseconds = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+		double averageTime = elapsedMicroseconds / ((double) numExceptions);
+
+		printf("Exceptions Occurred: %d\n", numExceptions);
+		printf("Total Elapsed Time: %ld microseconds\n", elapsedMicroseconds);
+		printf("Average Time Per Exception: %f microseconds\n", averageTime);
+
+		exit(0);
+	}
+
+	return;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+	signal(SIGFPE, handle_sigfpe); // Register signal handler
 
-	/*
-	 *
-	 * Implement Here
-	 *
-	 *
-	 */
+	int x = 5;
+	int y = 0;
+	int z = 0;
 
+	gettimeofday(&start, NULL); // start timer
+	z = x / y; // invoke SIGFPE
 	return 0;
-
 }
