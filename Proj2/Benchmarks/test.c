33057de113140570feda70886e18ee56c5c98e94
Sakib Rasul <rhythms06@gmail.com>
2020-10-23 00:29:29 -0300

    Improve joining test

--- Proj2/Benchmarks/test.c
+++ Proj2/Benchmarks/test.c
@@ -11,24 +11,35 @@
  * This will not be graded.
  */
 
- static void* f1(void* arg) {
-	 printf("Hi, I'm function 'f1'.");
-	 return arg;
- }
+mypthread_t t1;
+mypthread_t t2;
+
+static void* f1(void* arg) {
+  printf("Hi, I'm function 'f1'.");
+  void* returnValue;
+  mypthread_join(t2, &returnValue);
+  printf("The thread returned the value: %s\n", (char*) returnValue);
+  free(returnValue);
+  return arg;
+}
+
+static void* f2(void* arg) {
+  printf("Hi, I'm function 'f2'.");
+  void* returnValue;
+  mypthread_join(t1, &returnValue);
+  printf("The thread returned the value: %s\n", (char*) returnValue);
+  free(returnValue);
+  return arg;
+}
 
 int main(int argc, char **argv) {
 	mypthread_t t1;
-	void** ret;
 	mypthread_create(&t1, NULL, &f1, "f1 has returned.");
   printf("Added thread %u to CPU.\n", t1);
 
-  mypthread_create(&t1, NULL, &f1, "f1 has returned.");
-  printf("Added thread %u to CPU.\n", t1);
+  mypthread_t t2;
+  mypthread_create(&t2, NULL, &f2, "f2 has returned.");
+  printf("Added thread %u to CPU.\n", t2);
 
-	void* returnValue;
-	mypthread_join(t1, &returnValue);
-	printf("The thread returned the value: %s\n", (char*) returnValue);
-	free(returnValue);
-	mypthread_join(t1, ret);
 	return 0;
 }
