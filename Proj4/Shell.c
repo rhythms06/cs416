df6d4fcc65a49d9241bd178fbfe17e1d6200a9be
Sakib Rasul <rhythms06@gmail.com>
2020-12-09 23:41:38 -0400

    Add comments

--- Proj4/Shell.c
+++ Proj4/Shell.c
@@ -3,7 +3,15 @@
 #include <printf.h>
 #include <string.h>
 #include <stdbool.h>
-
+/* A custom shell.
+ * Acceptable commands include:
+ * [command options]
+ * [command options] | [command options]
+ * [command options] > [command options]
+ * [command options] >> [command options]
+ * cd
+ * exit
+*/
 int main() {
     // input holds the next line read from stdin
     char input[256] = "";
@@ -11,19 +19,15 @@ int main() {
     bool exit = false;
     // While the user hasn't hit "exit", execute their wishes!
     while (exit == false) {
-        // Print the current working directory
+        // Print the current working directory, cwd[i]
+        // Note: A default directory is printed if the current pathname is more than i characters long.
         char cwd[256] = "Current/Pathname/Is/Too/Long";
         getcwd(cwd, 256);
         printf("%s $ ", cwd);
-        // Retrieve input
+        // Retrieve input.
         fgets(input, 256, stdin);
-        /*
-         * commands = strtok \n and ;
-         * for token in commands:
-         *  if token is pipe { pipes... }
-         *  else if token is redirection { dup2... }
-         *  else execvp( strtok " " )
-         */
+        // Tokenize input into commands[i][j] with { ; \n } delimiters.
+        // Note: There can be at most i commands, and each command can be at most j characters long.
         char commands[256][256];
         char* command = strtok(input, ";\n");
         strcpy(commands[0], command);
@@ -34,11 +38,12 @@ int main() {
             i++;
             command = strtok(NULL, ";\n");
         }
-        // Iterate through the inputted commands
+        // Iterate over commands
         for (int j = 0; j <= i; j++) {
             if (strcmp(commands[j], "exit") != 0) {
                 printf("TODO: Execute \"%s\"\n", commands[j]);
             } else {
+                // The current command is "exit."
                 exit = true;
                 break;
             }
